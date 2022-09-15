2decd6798fd125e4d37b8936e67e80348a4aef3a
iamantonreznik <112612414+iamantonreznik@users.noreply.github.com>
2022-09-15 15:20:17 +0500

    startmessage, configparser added

--- logger_lib.py
+++ logger_lib.py
@@ -1,11 +1,31 @@
 from datetime import datetime
+import configparser
 
+
+# READ CONFIG FILE
+config = configparser.ConfigParser()
+config.read("config.cfg")
+
+# READ VARIABLES
+logfilename = config["logger_lib"]["logfilename"]
+logfilename_dateformat = config["logger_lib"]["logfilename_dateformat"]
+logfile_timeformat = config["logger_lib"]["logfile_timeformat"]
+
+startmessage = '\n' \
+               '|--------------------------------------|\n' \
+               '|          logger_lib started          |\n' \
+               '|          ' + datetime.today().strftime("%d-%m-%Y %H:%M:%S") +  '         |\n' \
+               '|--------------------------------------|\n\n'
 
 class Logger():
 
     def __init__(self):
-        self.logfile = 'log_' + str(datetime.now().date()) + '.txt'
+        self.logfile = logfilename + datetime.today().strftime(logfilename_dateformat) + '.txt'
+
+    def startLogging(self):
+        with open(self.logfile, "a") as f:
+            f.write(startmessage)
 
     def write(self, message):
         with open(self.logfile, "a") as f:
-            f.write(message + '\n')
+            f.write(datetime.today().strftime(logfile_timeformat)+ '   ' + message + '\n')
