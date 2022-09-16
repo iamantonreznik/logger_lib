168c56a6fefe625022b2a0f6406a03ea2f08c997
iamantonreznik <112612414+iamantonreznik@users.noreply.github.com>
2022-09-16 11:41:49 +0500

    added max log file size check

--- logger_lib.py
+++ logger_lib.py
@@ -1,31 +1,60 @@
 from datetime import datetime
 import configparser
+import os
 
 
 # READ CONFIG FILE
 config = configparser.ConfigParser()
-config.read("config.cfg")
+config.read("logger.cfg")
 
 # READ VARIABLES
-logfilename = config["logger_lib"]["logfilename"]
+logfileprefix = config["logger_lib"]["logfileprefix"]
+logfileextension = config["logger_lib"]["logfileextension"]
 logfilename_dateformat = config["logger_lib"]["logfilename_dateformat"]
 logfile_timeformat = config["logger_lib"]["logfile_timeformat"]
+maxlogfilesize = config["logger_lib"]["maxlogfilesize"]
 
-startmessage = '\n' \
-               '|--------------------------------------|\n' \
-               '|          logger_lib started          |\n' \
-               '|          ' + datetime.today().strftime("%d-%m-%Y %H:%M:%S") +  '         |\n' \
-               '|--------------------------------------|\n\n'
+startmessage =    '|--------------------------------------|\n' \
+                  '|          logger_lib started          |\n' \
+                  '|          ' + datetime.today().strftime("%d-%m-%Y %H:%M:%S") +  '         |\n' \
+                  '\n'
+endmessage =      '\n' \
+                  '|          logger_lib stopped          |\n' \
+                  '|--------------------------------------|\n\n'
 
 class Logger():
 
     def __init__(self):
-        self.logfile = logfilename + datetime.today().strftime(logfilename_dateformat) + '.txt'
+        self.indexnumber = 1
+        self.indexfilenumber = 0
+
+    def checkLogfile(self):
+        global logfile
+        indexfilenumber = 0
+        logfile = logfileprefix + datetime.today().strftime(logfilename_dateformat) + '_' + str(indexfilenumber) + logfileextension
+        if os.path.exists(logfile) and int('%2.0f' % (os.path.getsize(logfile) / 1024)) >= int(maxlogfilesize):
+            while os.path.exists(logfile):
+                indexfilenumber += 1
+                logfile = logfileprefix + datetime.today().strftime(logfilename_dateformat) + '_' + str(indexfilenumber) + logfileextension
+                if os.path.exists(logfile) and int('%2.0f' % (os.path.getsize(logfile) / 1024)) < int(maxlogfilesize):
+                    return logfile
+            else:
+                with open(logfile, "a") as f:
+                    f.write('')
+                return logfile
+        return logfile
 
     def startLogging(self):
-        with open(self.logfile, "a") as f:
+        self.checkLogfile()
+        with open(logfile, "a") as f:
             f.write(startmessage)
 
     def write(self, message):
-        with open(self.logfile, "a") as f:
-            f.write(datetime.today().strftime(logfile_timeformat)+ '   ' + message + '\n')
+        self.checkLogfile()
+        with open(logfile, "a") as f:
+            f.write('[ ' + str('%03d' % (self.indexnumber)) + ' ]  ' + datetime.today().strftime(logfile_timeformat) + '  ' + message + '\n')
+            self.indexnumber += 1
+
+    def __del__(self):
+        with open(logfile, "a") as f:
+            f.write(endmessage)
