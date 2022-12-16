8a224f955e159553f19a46a42f4361b39a945afd
iamantonreznik <112612414+iamantonreznik@users.noreply.github.com>
2022-12-16 16:39:28 +0100

    update

--- logger_lib.py
+++ logger_lib.py
@@ -3,16 +3,27 @@
 
 import os
 import sys
+import inspect
 from datetime import datetime
 
 
-def exception_handler(function):
+def exception_worker(function):
     def inner_function(*args, **kwargs):
+        callerframerecord = inspect.stack()[-1]
+        frame = callerframerecord[0]
+        info = inspect.getframeinfo(frame)
+
         try:
             function(*args, **kwargs)
+
+        except TypeError:
+            print('[' + os.path.basename(sys.argv[0]) + '] [ line: ' + str(info.lineno)
+                  + ' ] logger_lib usage error --> ' + str(info.code_context[0].removesuffix('\n')))
+
         except OSError as logger_error:
-            print('[' + os.path.basename(sys.argv[0]) + '] logger_lib writing error: '
-                  + logger_error.strerror + ' --> ' + logger_error.filename)
+            print('[' + os.path.basename(sys.argv[0]) + '] [ line: ' + str(info.lineno)
+                  + ' ] logger_lib writing error: ' + logger_error.strerror +
+                  ' --> ' + logger_error.filename)
 
     return inner_function
 
@@ -26,8 +37,9 @@ class Logger:
             self.prefix = self.prefix + "_"
         self.logfile = os.path.join(self.folder, self.prefix + datetime.today().strftime("%d-%m-%Y") + ".log")
 
-    @exception_handler
+    @exception_worker
     def __make_log_file(self):
+        logfile = self.logfile
 
         if not os.path.exists(self.folder):
             os.mkdir(self.folder)
@@ -35,42 +47,39 @@ class Logger:
             with open(self.logfile, "a") as f:
                 f.write('')
 
-        # return self.logfile
+        return logfile
 
     def __message_worker(self, level, message):
-        message_levels = {'info': '  [ INFO ]  ',
-                          'debug': '  [ DEBUG ]  ',
-                          'error': '  [ ERROR ]  ',
+        message_levels = {'info':     '  [ INFO ]  ',
+                          'debug':    '  [ DEBUG ]  ',
+                          'error':    '  [ ERROR ]  ',
                           'critical': '  --> CRITICAL <--  '}
 
         log_message = '[ ' + datetime.today().strftime('%H:%M:%S') + ' ]  ' + \
                       '[ ' + os.path.basename(sys.argv[0]) + ' ]' + message_levels[level] + message + '\n'
         return log_message
 
-    @exception_handler
+    @exception_worker
     def write(self, message):
         self.__make_log_file()
 
         with open(self.logfile, "a") as f:
             f.write(self.__message_worker('info', message))
 
-    @exception_handler
+    @exception_worker
     def debug(self, message):
         self.__make_log_file()
-
         with open(self.logfile, "a") as f:
             f.write(self.__message_worker('debug', message))
 
-    @exception_handler
+    @exception_worker
     def error(self, message):
         self.__make_log_file()
-
         with open(self.logfile, "a") as f:
             f.write(self.__message_worker('error', message))
 
-    @exception_handler
+    @exception_worker
     def critical(self, message):
         self.__make_log_file()
-
         with open(self.logfile, "a") as f:
             f.write(self.__message_worker('critical', message))
